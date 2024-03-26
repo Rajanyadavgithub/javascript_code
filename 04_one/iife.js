9006c276f4679f1d668ef9e4f9a2204fbd309aa3
Rajanyadavgithub <147150311+Rajanyadavgithub@users.noreply.github.com>
2024-03-26 10:09:25 +0000

    immediate invoked function expression (iife)

---
+++ 04_one/iife.js
@@ -0,0 +1,20 @@
+//IIFE  (immediate invoked function expression)
+
+// (function chai(){
+//     //console.log(`DB CONNECTED`)
+// })();
+//chai()
+
+
+//  syntax of iifi is whole funtion with in parenthesis after we call funtion with parenthesis like (funtion)(call funtion)
+
+// iifi funtion is used to run emmidiately and to remove global scope polution (variabal and diclaration) .
+
+// (()=>{
+//     console.log(`DB CONNECTED`);
+// })();
+// ((name)=>{
+//     console.log(`DB CONNECTED TO ${name}`)
+// })("Rajan");
+
+
