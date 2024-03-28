ed02e836f53779ca5e494adbf72fe498c4e2d1fd
Rajanyadavgithub <147150311+Rajanyadavgithub@users.noreply.github.com>
2024-03-28 04:58:37 +0000

    iterations

--- 04_one/itrations.js
+++ 04_one/itrations.js
@@ -49,3 +49,17 @@ while(index<=10){
     index= index+2
 }
 
+let myArrays = ["flash","batman","superman"]
+ let arr=0
+ while(arr<myArray.length){
+    console.log(`value is ${myArrays[arr]}`)
+    arr=arr+1
+
+ }
+
+ let score= 1
+ do{
+    console.log(`score is ${score}`)
+    score++
+ }
+ while(score<=10);
